427ba9177b2d98c1894f6c154711764317567727
Ever <daftsure9@gmail.com>
2026-03-31 13:32:02 -0600

    Listas las funciones base que permite conocer los productos que hacen falta

--- UsandoPandas.py
+++ UsandoPandas.py
@@ -2,21 +2,46 @@ import pandas as pd
 import os
 def ubicacionArchivo ():
     #Abriendo el archivo Reporte
-    usuario=os.path.join("~")
+    usuario=os.path.expanduser("~")
     rutaArchivo=os.path.join(usuario,"Desktop","PedidosApp","Reporte.xlsx")
-    df=pd.read_excel(rutaArchivo)
-    return df
-
-def ReporteCocaCola ():
-    #Abriendo el archivo Reporte
-    df=ubicacionArchivo()
-    #Aplicando las primeras condiciones
-    NombreProductosMinimo=df.loc[df["Stock"]<=df["Stock Minimo"],["Nombre Completo", "Stock"]]
-    if not NombreProductosMinimo.empty:
-        print("REPORTE DE PEDIDO COCACOLA")
-        print("-"*50)
-        print(NombreProductosMinimo)
-        print("-"*50)
+    if os.path.exists(rutaArchivo):
+        df=pd.read_excel(rutaArchivo)
+        return df
     else:
-        print("No hay productos por pedir")
-ReporteCocaCola()
+        print("No hay ningun archivo ")
+        return None
+
+def reportePepsiCo ():
+    df=ubicacionArchivo()
+    if df is not None:
+        #Listando todos los productos con marca "PepsiCo"
+        pepsi_df=df[df["Marca"].str.upper()=="PEPSICO"]
+        if pepsi_df.empty:
+            print("No se a encontrado registro para PepsiCo")
+        else:
+            #Aplicando la condiciones principal (bajo stock)
+            filtroPepsi=(df["Marca"].str.upper()=="PEPSICO") & (df["Stock"]<=df["Stock Minimo"])
+            bajoStockPepsi=df[filtroPepsi]
+            if bajoStockPepsi.empty:
+                print("Tienes productos suficientes")
+            else:
+                #Debe conocer los elementos bajo stock para poder imprimir los que hagan falta
+                print("-->PEDIDO PARA PEPSI\n")
+                print(bajoStockPepsi[["Nombre", "Piezas"]].to_markdown(index=False))
+reportePepsiCo()
+
+def reporteCocaCola ():
+    df=ubicacionArchivo()
+    if df is not None:
+        cocaCola_df=df[df["Marca"].str.upper()=="COCACOLA"]
+        if cocaCola_df.empty:
+            print("No se a encontrado registro para CocaCola")
+        else:
+            filtroCocaCola=(df["Marca"].str.upper()=="COCACOLA") & (df["Stock"]<=df["Stock Minimo"])
+            bajoStockCocaCola=df[filtroCocaCola]
+            if bajoStockCocaCola.empty:
+                print("Tienes productos suficentes")
+            else:
+                print("-->PEDIDO PARA COCACOLA\n")
+                print(bajoStockCocaCola[["Nombre", "Piezas"]].to_markdown(index=False))
+reporteCocaCola()
